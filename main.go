7597af6f3219360850cc39ccddd227dc573a42d3
xcosk <maks.m90.00@mail.ru>
2026-02-06 22:34:24 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "sync"
-)
-
 func main() {
-    var wg sync.WaitGroup
-    wg.Add(5)
 
-    for i := 0; i < 5; i++ {
-        go func() {
-            fmt.Println(i)
-            wg.Done()
-        }()
-    }
-
-    wg.Wait()
 }
 
 func MaxInt(a, b int) int {
@@ -26,3 +11,38 @@ func MaxInt(a, b int) int {
 
     return b
 }
+
+
+
+
+
+
+
+// package main
+
+// import (
+//     "fmt"
+//     "sync"
+// )
+
+// func main() {
+//     var wg sync.WaitGroup
+//     wg.Add(5)
+
+//     for i := 0; i < 5; i++ {
+//         go func() {
+//             fmt.Println(i)
+//             wg.Done()
+//         }()
+//     }
+
+//     wg.Wait()
+// }
+
+// func MaxInt(a, b int) int {
+//     if a >= b {
+//         return a
+//     }
+
+//     return b
+// }
